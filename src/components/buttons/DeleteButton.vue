57a2b54da5d1ea56145320a674c1f74ef13bb121
CosinNikola <nikola.cosin123@gmail.com>
2022-04-19 20:18:45 +0200

    first commit

---
+++ src/components/buttons/DeleteButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn">Obrisi</button>
+</template>
+
+<script>
+export default {
+    props: ['path']
+}
+</script>
+
+<style scoped>
+.btn{
+        margin: 10px 0;
+        cursor: pointer;
+        padding: 10px 15px;
+        border: 0;
+        border-radius: 5px;
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+        font-size: 15px;
+        background: crimson;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,73 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Lokal from '../views/Lokal.vue'
+import Meni from '../views/Meni.vue'
+import Admin from '../views/Admin.vue'
+import EditProizvod from '../views/EditProizvod.vue'
+import AdminMeni from '../views/AdminMeni.vue'
+import AdminStolovi from '../views/AdminStolovi.vue'
+import AdminKorisnici from '../views/AdminKorisnici.vue'
+import EditKorisnik from '../views/EditKorisnik.vue'
+import CreateKorisnik from '../views/CreateKorisnik.vue'
+import EditStolovi from '../views/EditStolovi.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/lokal',
+    name: 'Lokal',
+    component: Lokal
+  },
+  {
+    path: '/meni',
+    name: 'Meni',
+    component: Meni
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/editProizvod/:id',
+    name: 'EditProizvod',
+    component: EditProizvod
+  },
+  {
+    path: '/admin/meni',
+    name: 'AdminMeni',
+    component: AdminMeni
+  },
+  {
+    path: '/admin/stolovi',
+    name: 'AdminStolovi',
+    component: AdminStolovi
+  },
+  {
+    path: '/admin/korisnici',
+    name: 'AdminKorisnici',
+    component: AdminKorisnici
+  },
+  {
+    path: '/admin/korisnici/edit/:id',
+    name: 'EditKorisnik',
+    component: EditKorisnik
+  },
+  {
+    path: '/admin/korisnici/create',
+    name: 'CreateKorisnik',
+    component: CreateKorisnik
+  },
+  {
+    path: '/admin/stolovi/edit/:id',
+    name: 'EditStolovi',
+    component: EditStolovi
+  },
+
 ]
 
 const router = createRouter({
